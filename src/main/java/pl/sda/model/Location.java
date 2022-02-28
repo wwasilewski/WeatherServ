b1a29a822c829e87f72cb620e6ebf5dd91069b72
Wojciech <w.wasilewski@gmail.com>
2022-02-28 15:09:16 +0100

    fixing Location.java

--- src/main/java/pl/sda/model/Location.java
+++ src/main/java/pl/sda/model/Location.java
@@ -6,19 +6,29 @@ import java.util.UUID;
 public class Location {
 
     private String id = UUID.randomUUID().toString();
+    private String name;
     private String region;
-    private String countryName;
-    private Coord coordinates;
+    private String country;
+    private Coord coord;
     private Weather main;
 
-    public Location(String region, String countryName, Coord coordinates) {
+    public Location(String name, String region, String country, Coord coord) {
+        this.name = name;
         this.region = region;
-        this.countryName = countryName;
-        this.coordinates = coordinates;
+        this.country = country;
+        this.coord = coord;
     }
 
     public String getId() {
         return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getRegion() {
@@ -29,20 +39,20 @@ public class Location {
         this.region = region;
     }
 
-    public String getCountryName() {
-        return countryName;
+    public String getCountry() {
+        return country;
     }
 
-    public void setCountryName(String countryName) {
-        this.countryName = countryName;
+    public void setCountry(String country) {
+        this.country = country;
     }
 
-    public Coord getCoordinates() {
-        return coordinates;
+    public Coord getCoord() {
+        return coord;
     }
 
-    public void setCoordinates(Coord coordinates) {
-        this.coordinates = coordinates;
+    public void setCoord(Coord coord) {
+        this.coord = coord;
     }
 
     public Weather getMain() {
@@ -58,24 +68,25 @@ public class Location {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Location location = (Location) o;
-        return Objects.equals(getId(), location.getId()) && Objects.equals(getRegion(),
-                location.getRegion()) && Objects.equals(getCountryName(), location.getCountryName())
-                && Objects.equals(getCoordinates(), location.getCoordinates())
+        return Objects.equals(getId(), location.getId()) && Objects.equals(getName(), location.getName())
+                && Objects.equals(getRegion(), location.getRegion()) && Objects.equals(getCountry(),
+                location.getCountry()) && Objects.equals(getCoord(), location.getCoord())
                 && Objects.equals(getMain(), location.getMain());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getRegion(), getCountryName(), getCoordinates(), getMain());
+        return Objects.hash(getId(), getName(), getRegion(), getCountry(), getCoord(), getMain());
     }
 
     @Override
     public String toString() {
         return "Location{" +
                 "id='" + id + '\'' +
+                ", name='" + name + '\'' +
                 ", region='" + region + '\'' +
-                ", countryName='" + countryName + '\'' +
-                ", coordinates=" + coordinates +
+                ", country='" + country + '\'' +
+                ", coord=" + coord +
                 ", main=" + main +
                 '}';
     }
