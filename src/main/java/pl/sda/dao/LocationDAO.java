47c6d6149dadda2380ec55950d52c8e08b70ff93
Kasia <sawicka.katarzyna91@gmail.com>
2022-03-10 11:02:20 +0100

    add DAO classes

---
+++ src/main/java/pl/sda/dao/LocationDAO.java
@@ -0,0 +1,16 @@
+package pl.sda.dao;
+
+import pl.sda.model.Location;
+
+import java.util.List;
+
+public class LocationDAO {
+
+    public static Location saveLocation() {
+        return null;
+    }
+
+    public static List<Location> showAllLocations() {
+        return null;
+    }
+}
--- src/main/java/pl/sda/service/WeatherService.java
+++ src/main/java/pl/sda/service/WeatherService.java
@@ -1,14 +1,9 @@
 package pl.sda.service;
 
 import com.google.gson.Gson;
-import pl.sda.model.Coord;
-import pl.sda.model.Location;
 import pl.sda.model.Weather;
-import pl.sda.model.Wind;
-import pl.sda.model.openWeatherAPI.OpenWeatherObject;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
@@ -16,27 +11,6 @@ import java.nio.file.StandardOpenOption;
 public class WeatherService {
 
     private static final String PATH = "src/main/resources/weather.json";
-    private static final int FORECAST_FOR_TOMORROW = 1;
-
-
-    public static Weather readWeatherForSpecificDay(OpenWeatherObject weatherObject, int dayOfForecast) {
-        Weather result = new Weather();
-
-        if (dayOfForecast >= 0 && dayOfForecast <= 7) {
-            result.setTemp(weatherObject.getDaily()[dayOfForecast].getTemp().getDay());
-            result.setPressure(weatherObject.getDaily()[dayOfForecast].getPressure());
-            result.setHumidity(weatherObject.getDaily()[dayOfForecast].getHumidity());
-            result.setWind(new Wind(weatherObject.getDaily()[dayOfForecast].getWind_speed(),
-                    weatherObject.getDaily()[dayOfForecast].getWind_deg()));
-        } else {
-            result.setTemp(weatherObject.getDaily()[FORECAST_FOR_TOMORROW].getTemp().getDay());
-            result.setPressure(weatherObject.getDaily()[FORECAST_FOR_TOMORROW].getPressure());
-            result.setHumidity(weatherObject.getDaily()[FORECAST_FOR_TOMORROW].getHumidity());
-            result.setWind(new Wind(weatherObject.getDaily()[FORECAST_FOR_TOMORROW].getWind_speed(),
-                    weatherObject.getDaily()[FORECAST_FOR_TOMORROW].getWind_deg()));
-        }
-        return result;
-    }
 
     public static void saveWeatherToJSONFile(Weather weather) {
         Gson gson = new Gson();
