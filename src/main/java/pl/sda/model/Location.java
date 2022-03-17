3c3e55d1aa45f588a5f52e4b644a4e6fe2ec7ad9
Wojciech <w.wasilewski@gmail.com>
2022-03-17 11:51:18 +0100

    adding hibernate entities + mapping (in progress)

--- src/main/java/pl/sda/model/Location.java
+++ src/main/java/pl/sda/model/Location.java
@@ -4,17 +4,40 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "locations")
 public class Location {
 
-    private String id = UUID.randomUUID().toString();
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "location_id")
+    private Integer id;
+
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "region")
     private String region;
+
+    @Column(name = "country")
     private String country;
+
+    @Column(name = "longitude")
     private float longitude;
+
+    @Column(name = "latitude")
     private float latitude;
+
+    @ManyToOne
+    private Weather weather;
+
+
 }
--- src/main/java/pl/sda/model/Weather.java
+++ src/main/java/pl/sda/model/Weather.java
@@ -4,18 +4,36 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.UUID;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "weathers")
 public class Weather {
 
-    private String id = UUID.randomUUID().toString();
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "weather_id")
+    private Integer id;
+
+    @Column(name = "temp")
     private float temp;
+
+    @Column(name = "pressure")
     private float pressure;
+
+    @Column(name = "humidity")
     private int humidity;
+
+    @Column(name = "wind_speed")
     private float windSpeed; //wind speed
+
+    @Column(name = "wind_dir")
     private float windDeg; //wind direction
+
+    @Column(name = "timestamp")
     private long timestamp;
 }
