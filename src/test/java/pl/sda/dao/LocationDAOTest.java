8ba98b32477be4e421ac3167788833ef930d36a7
Kasia <sawicka.katarzyna91@gmail.com>
2022-03-18 19:01:27 +0100

    add all methods in LocationDAO.java

---
+++ src/test/java/pl/sda/dao/LocationDAOTest.java
@@ -0,0 +1,67 @@
+package pl.sda.dao;
+
+import org.junit.jupiter.api.Test;
+import pl.sda.model.Location;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LocationDAOTest {
+
+    private LocationDAO locationDAO = new LocationDAO();
+
+    @Test
+    void shouldFindByCityNameIfExists() {
+        Location location = new Location();
+        location.setName("imie1");
+        location.setCountry("PL");
+        locationDAO.saveLocation(location);
+        String name = "imie1";
+        Location foundLocation = locationDAO.findByName(name);
+        assertThat(foundLocation.getName()).isEqualTo(name);
+    }
+
+    @Test
+    void shouldNotFindByCityNameIfDoesntExists() {
+        String name = "random";
+        Location foundLocation = locationDAO.findByName(name);
+        assertThat(foundLocation).isNull();
+    }
+
+    @Test
+    void doNotSaveLocationIfCityNameIsEmpty() {
+    }
+
+    @Test
+    void doNotSaveLocationIfCountryNameIsEmpty() {
+    }
+
+    @Test
+    void saveLocationIfRegionIsNull() {
+    }
+
+    @Test
+    void doNotSaveLocationIfCoordinatesAreIncorrect() {
+    }
+
+    @Test
+    void saveLocationIfInputIsCorrect() {
+    }
+
+    @Test
+    void findAllLocationsEmptyList() {
+        List<Location> allLocations = locationDAO.findAllLocations();
+        assertThat(allLocations).isEmpty();
+    }
+
+    @Test
+    void findAllLocationsOneLocation() {
+        Location location = new Location();
+        location.setName("imie1");
+        location.setCountry("PL");
+        locationDAO.saveLocation(location);
+        List<Location> allLocations = locationDAO.findAllLocations();
+        assertThat(allLocations.size()).isEqualTo(1);
+    }
+}
